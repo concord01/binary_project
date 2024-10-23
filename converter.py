497291751d9ec52dc5f7ee104956224a41f6b59a
Joaquin Hoyem <joaquin.hoyem@gmail.com>
2024-10-23 18:11:59 +0000

    final submission thing!

--- converter.py
+++ converter.py
@@ -3,34 +3,36 @@ def converter(input):
     index = 0
     inputChange = input
     indexFinal = 0
-    # getting array to correct size
-    while True:
-        if (inputChange - (2 ** index) >= 0):
-            index = index + 1
-            output.append(0)
-        else:
-            indexFinal = index
-            break
-    # correcting the index
-    index = 0
-    indexFinal = indexFinal-1
+    if input == 0:
+        output.append(0)
+    else:
+        # getting array to correct size
+        while True:
+            if (inputChange - (2 ** index) >= 0):
+                index = index + 1
+                output.append(0)
+            else:
+                indexFinal = index
+                break
+        # correcting the index
+        index = 0
+        indexFinal = indexFinal-1
 
-    # going and adding adding the 1s into the right spot
-    while indexFinal >= 0:
-        # decides if it should add a 1 or not to a given index
-        if (inputChange - (2 ** indexFinal) >= 0):
-            output[index] = 1
-            index = index + 1
-            inputChange = inputChange - (2 ** indexFinal)
-            indexFinal = indexFinal - 1
-        else:
-            # skips index
-            index = index + 1
-            indexFinal = indexFinal - 1
+        # going and adding adding the 1s into the right spot
+        while indexFinal >= 0:
+            # decides if it should add a 1 or not to a given index
+            if (inputChange - (2 ** indexFinal) >= 0):
+                output[index] = 1
+                index = index + 1
+                inputChange = inputChange - (2 ** indexFinal)
+                indexFinal = indexFinal - 1
+            else:
+                # skips index
+                index = index + 1
+                indexFinal = indexFinal - 1
     return output
-        
-    
 
 
 
-print(converter(1985))
+
+print(converter(2008))
